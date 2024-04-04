7dc77a4430d6d8d5b8418f4e0ca21e60c9965ddc
FrancoMusolino <francomusolino55@gmail.com>
2024-04-04 14:46:32 -0300

    (render) use render external url on prod

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,20 +1,3 @@
 module.exports = ({ env }) => ({
-  proxy: true,
-  host: "0.0.0.0",
-  port: process.env.PORT,
-  // url: env("MY_HEROKU_URL"),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET"),
-    },
-  },
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-    defaultHeaders: {
-      Authorization: `Bearer ${env("WEBHOOK_TOKEN", undefined)}`,
-    },
-  },
+  url: env("RENDER_EXTERNAL_URL"),
 });
