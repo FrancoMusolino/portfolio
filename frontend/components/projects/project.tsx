b6420a7236d03bf8692667fbb07cc3a6fec50487
FrancoMusolino <francomusolino55@gmail.com>
2024-04-10 12:29:36 -0300

    (projects) adjust project card styles

--- frontend/components/projects/project.tsx
+++ frontend/components/projects/project.tsx
@@ -36,9 +36,9 @@ export function ProjectCard({
       }}
       className="group mb-3 sm:mb-8 last:mb-0"
     >
-      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-          <h3 className="text-2xl font-semibold">{title}</h3>
+      <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:px-8 relative sm:h-[20rem] hover:bg-gray-200 transition  dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="pt-4 pb-7 px-5 sm:px-0 sm:group-even:pl-10 sm:group-odd:pr-10 sm:pt-10 sm:max-w-[55%] flex flex-col h-full sm:group-even:ml-[16rem]">
+          <h3 className="text-xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
